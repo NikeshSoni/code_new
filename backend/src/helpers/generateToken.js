acacb54ef0966404f0f5cc52eadcd9f18a685912
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-01-17 15:29:08 +0530

    Working on the JWT Tocken for the Login and Sigin user

--- backend/src/helpers/generateToken.js
+++ backend/src/helpers/generateToken.js
@@ -2,6 +2,9 @@ import jwt from "jsonwebtoken";
 
 //use user id to generate token
 const generateToken = (id) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in the environment variables");
+  }
   // token must be returned to the client
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createTask } from "../controllers/task/taskController.js";
-import {protect} from "../middleware/authMiddleware.js"
-const router =  express.Router();
+import { protect } from "../middleware/authMiddleware.js"
+const router = express.Router();
 
 
-router.post("task/create" ,  protect, createTask)
+router.post("task/create", protect, createTask)
 
 
 export default router;
